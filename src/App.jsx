0952cba33f127e002687f6e4af9e62cef14e80b0
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-01 22:06:30 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+
 function App() {
-  return <div>The Wild Oasis</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="bookings" element={<Bookings />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
