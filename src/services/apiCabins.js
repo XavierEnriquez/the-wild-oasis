13e10ebd1ca09591ee8650f5acedcdf9dac97431
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-02 23:14:13 -0600

    Create apiCabins.js

---
+++ src/services/apiCabins.js
@@ -0,0 +1,18 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  let { data, error } = await supabase.from("cabins").select("*");
+  if (error) {
+    console.log(error);
+    throw new Error("Error loading the cabins data");
+  }
+  return data;
+}
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.log(error);
+    throw new Error("Cabin could not be deleted");
+  }
+  return data;
+}
