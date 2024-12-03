f0203c391ea5a27320c1cca6843fbbd7aff25888
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-03 00:28:25 -0600

    Update Cabins.jsx

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,19 +1,31 @@
 import CabinTable from "../features/cabins/CabinTable";
-import { ContainerXl } from "../styles/containers";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import Button from "../ui/Button";
+import Container from "../ui/container";
+import ButtonGroup from "../ui/ButtonGroup";
 
 function Cabins() {
   return (
-    <ContainerXl>
-      <Row type="horizontal">
-        <Heading as="h1">All cabins</Heading>
-        <div>Filter / Sort</div>
-      </Row>
+    <Container size="xl">
       <Row>
+        <Row type="horizontal">
+          <Heading as="h1">All cabins</Heading>
+          <ButtonGroup>
+            <Button size="medium" variation="secondary">
+              Filter
+            </Button>
+            <Button size="medium" variation="secondary">
+              Filter
+            </Button>
+          </ButtonGroup>
+        </Row>
         <CabinTable />
+        <div>
+          <Button>Add new cabin</Button>
+        </div>
       </Row>
-    </ContainerXl>
+    </Container>
   );
 }
 
