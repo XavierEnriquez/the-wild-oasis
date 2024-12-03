744e76b63fa522961035110bd1cd1a4a3a3eec09
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-02 23:14:32 -0600

    Create containers.js

---
+++ src/styles/containers.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ContainerMd = styled.div`
+  max-width: 60rem;
+  margin-right: auto;
+`;
+export const ContainerLg = styled.div`
+  max-width: 90rem;
+  margin-right: auto;
+`;
+export const ContainerXl = styled.div`
+  max-width: 120rem;
+  margin-right: auto;
+`;
