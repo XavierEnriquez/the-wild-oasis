e35cbe2f79b8a6bffd07a5fc77688f1d8759cbe1
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-04 17:05:11 -0600

    Update Settings.jsx

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,7 +1,17 @@
 import Heading from "../ui/Heading";
+import Container from "../ui/Container";
+import Row from "../ui/Row";
+import UpdateSettingsForm from "../features/settings/UpdateSettingsForm";
 
 function Settings() {
-  return <Heading as="h1">Update hotel settings</Heading>;
+  return (
+    <Container>
+      <Row>
+        <Heading as="h1">Update hotel settings</Heading>
+        <UpdateSettingsForm />
+      </Row>
+    </Container>
+  );
 }
 
 export default Settings;
