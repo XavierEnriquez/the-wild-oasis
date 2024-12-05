d53e5a61dc3f95d046234b56dec3b0793881943f
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-05 13:12:29 -0600

    Update Cabins.jsx

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,36 +1,27 @@
-import { useState } from "react";
-
 import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import Button from "../ui/Button";
 import Container from "../ui/Container";
-import ButtonGroup from "../ui/ButtonGroup";
-import CreateUpdateCabinForm from "../features/cabins/CreateUpdateCabinForm";
+import CabinAdd from "../features/cabins/CabinAdd";
 
 function Cabins() {
-  const [showForm, setShowForm] = useState(false);
   return (
     <Container size="large">
       <Row>
         <Row type="horizontal">
           <Heading as="h1">All cabins</Heading>
-          <ButtonGroup>
+          <div>
             <Button size="medium" variation="secondary">
               Filter
             </Button>
             <Button size="medium" variation="secondary">
-              Filter
+              Sort
             </Button>
-          </ButtonGroup>
+          </div>
         </Row>
         <CabinTable />
-        <div>
-          <Button onClick={() => setShowForm((show) => !show)}>
-            Add new cabin
-          </Button>
-        </div>
-        {showForm && <CreateUpdateCabinForm />}
+        <CabinAdd />
       </Row>
     </Container>
   );
