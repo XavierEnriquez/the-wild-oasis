b102e2bf288d7355e74a7fcae0adc729f2d6a358
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-05 14:59:46 -0600

    Update CabinAdd.jsx

--- src/features/cabins/CabinAdd.jsx
+++ src/features/cabins/CabinAdd.jsx
@@ -1,23 +1,40 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import CreateUpdateCabinForm from "./CreateUpdateCabinForm";
 import Modal from "../../ui/Modal";
 
+// V-1 CabinAdd component
+// function CabinAdd() {
+//   const [isOpenModal, setIsOpenModal] = useState(false);
+//   return (
+//     <div>
+//       <div>
+//         <Button onClick={() => setIsOpenModal((show) => !show)}>
+//           Add new cabin
+//         </Button>
+//       </div>
+//       {isOpenModal && (
+//         <Modal onClose={() => setIsOpenModal(false)}>
+//           <CreateUpdateCabinForm onCloseModal={() => setIsOpenModal(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
+
+// V-2 CabinAdd as a Compound component
+// Multiple windows can be open by naming them each one differently and referencing them by name
 function CabinAdd() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
   return (
-    <div>
-      <div>
-        <Button onClick={() => setIsOpenModal((show) => !show)}>
-          Add new cabin
-        </Button>
-      </div>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateUpdateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open windowName="cabinadd-form">
+        <div>
+          <Button>Add new cabin</Button>
+        </div>
+      </Modal.Open>
+      <Modal.Window name="cabinadd-form">
+        <CreateUpdateCabinForm />
+      </Modal.Window>
+    </Modal>
   );
 }
 
