b5ed1d5cca4b3b61cea2cd3d70115aedcc0b7333
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-06 21:02:13 -0600

    Update useUpdateCabin.js

--- src/features/cabins/useUpdateCabin.js
+++ src/features/cabins/useUpdateCabin.js
@@ -5,12 +5,12 @@ import toast from "react-hot-toast";
 export function useUpdateCabin() {
   const queryClient = useQueryClient();
   const { mutate: updateCabin, isLoading: isUpdating } = useMutation({
-    mutationFn: ({ updateData, id }) => createUpdateCabinFn(updateData, id),
+    mutationFn: ({ id, newCabinData }) => createUpdateCabinFn(id, newCabinData),
     onSuccess: () => {
       toast.success("Cabin succesfully edited");
       queryClient.invalidateQueries({ queryKey: ["cabins"] });
     },
     onError: (err) => toast.error(err.message),
   });
-  return { isUpdating, updateCabin };
+  return { updateCabin, isUpdating };
 }
