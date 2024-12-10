f4f2b866c4b3160a4823ad36179c84f80b86b493
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-09 20:54:54 -0600

    Create CabinRow-v2.jsx

---
+++ src/features/cabins/CabinRow-v2.jsx
@@ -0,0 +1,139 @@
+import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+import { formatCurrency } from "../../utils/helpers";
+import { useDeleteCabin } from "./useDeleteCabin";
+import { useCreateCabin } from "./useCreateCabin";
+import Button from "../../ui/Button";
+import CreateUpdateCabinForm from "./CreateUpdateCabinForm";
+import ButtonGroup from "../../ui/ButtonGroup";
+import Modal from "../../ui/Modal";
+import ConfirmDelete from "../../ui/ConfirmDelete";
+import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
+
+const Img = styled.img`
+  display: block;
+  width: 6.4rem;
+  aspect-ratio: 3 / 2;
+  object-fit: cover;
+  object-position: center;
+  transform: scale(1.5) translateX(-7px);
+`;
+
+const Cabin = styled.div`
+  font-size: 1.6rem;
+  font-weight: 600;
+  color: var(--color-grey-600);
+  font-family: "Sono";
+`;
+
+const Price = styled.div`
+  font-family: "Sono";
+  font-weight: 600;
+`;
+
+const Discount = styled.div`
+  font-family: "Sono";
+  font-weight: 500;
+  color: var(--color-green-700);
+`;
+
+function CabinRow({ cabin }) {
+  const { createCabin, isCreating } = useCreateCabin();
+  const { isDeleting, deleteCabin } = useDeleteCabin();
+
+  const {
+    id: cabinId,
+    name,
+    maxCapacity,
+    regularPrice,
+    discount,
+    description,
+    image,
+  } = cabin;
+
+  function handleDuplicate() {
+    createCabin({
+      name: `${name}-copy`,
+      maxCapacity,
+      regularPrice,
+      discount,
+      description,
+      image,
+    });
+  }
+
+  return (
+    <>
+      <Table.Row>
+        <Img src={image} />
+        <Cabin>{name}</Cabin>
+        <p>Fits up to {maxCapacity} guests</p>
+        <Price>{formatCurrency(regularPrice)}</Price>
+        {discount ? (
+          <Discount>{formatCurrency(discount)}</Discount>
+        ) : (
+          <span>&mdash;</span>
+        )}
+        <ButtonGroup>
+          <Button
+            variation="secondary"
+            onClick={handleDuplicate}
+            disabled={isDeleting || isCreating}
+          >
+            <HiSquare2Stack />
+          </Button>
+          <Modal>
+            <Modal.Open windowName="update">
+              <Button variation="secondary" disabled={isDeleting || isCreating}>
+                <HiPencil />
+              </Button>
+            </Modal.Open>
+            <Modal.Window name="update">
+              <CreateUpdateCabinForm cabinToUpdate={cabin} />
+            </Modal.Window>
+
+            <Modal.Open windowName="delete">
+              <Button variation="secondary" disabled={isDeleting || isCreating}>
+                <HiTrash />
+              </Button>
+            </Modal.Open>
+            <Modal.Window name="delete">
+              <ConfirmDelete
+                resourceName="cabin"
+                onConfirm={() => deleteCabin(cabinId)}
+                disabled={isDeleting}
+              />
+            </Modal.Window>
+          </Modal>
+          <Menus.Menu>
+            <Menus.Toggle id={cabinId} />
+            <Menus.List id={cabinId}>
+              <Menus.Button onClick={handleDuplicate} icon={<HiSquare2Stack />}>
+                Duplicate
+              </Menus.Button>
+              <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
+              <Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
+            </Menus.List>
+          </Menus.Menu>
+        </ButtonGroup>
+      </Table.Row>
+    </>
+  );
+}
+
+CabinRow.propTypes = {
+  cabin: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    maxCapacity: PropTypes.number,
+    regularPrice: PropTypes.number,
+    discount: PropTypes.number,
+    description: PropTypes.string,
+    image: PropTypes.any,
+  }),
+};
+
+export default CabinRow;
