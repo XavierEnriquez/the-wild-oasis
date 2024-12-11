627c579c7789a237d78df95bd5525e7a274eae1c
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-10 23:33:17 -0600

    Create CabinTableOperations.jsx

---
+++ src/features/cabins/CabinTableOperations.jsx
@@ -0,0 +1,31 @@
+import TableOperations from "../../ui/TableOperations";
+import Filter from "../../ui/Filter";
+import SortBy from "../../ui/SortBy";
+
+function CabinTableOperations() {
+  return (
+    <TableOperations>
+      <Filter
+        filterParam="discount"
+        options={[
+          { value: "all", label: "all" },
+          { value: "with-discount", label: "By discount" },
+          { value: "no-discount", label: "No discount" },
+        ]}
+      />
+
+      <SortBy
+        options={[
+          { value: "name-asc", label: "Sort by name (A-Z)" },
+          { value: "name-desc", label: "Sort by name (Z-A)" },
+          { value: "regularPrice-asc", label: "Sort by price 🔺" },
+          { value: "regularPrice-desc", label: "Sort by price 🔻" },
+          { value: "maxCapacity-asc", label: "Sort by capacity 🔺" },
+          { value: "maxCapacity-desc", label: "Sort by capacity 🔻" },
+        ]}
+      />
+    </TableOperations>
+  );
+}
+
+export default CabinTableOperations;
