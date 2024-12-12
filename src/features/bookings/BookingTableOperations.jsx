41f3242aa42759505d9dab5a0b3ac9c746ba04c8
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-12 15:46:40 -0600

    Update BookingTableOperations.jsx

--- src/features/bookings/BookingTableOperations.jsx
+++ src/features/bookings/BookingTableOperations.jsx
@@ -6,24 +6,24 @@ function BookingTableOperations() {
   return (
     <TableOperations>
       <Filter
-        filterField="status"
+        filterParam="status"
         options={[
           { value: "all", label: "All" },
-          { value: "checked-out", label: "Checked out" },
-          { value: "checked-in", label: "Checked in" },
           { value: "unconfirmed", label: "Unconfirmed" },
+          { value: "checked-in", label: "Checked in" },
+          { value: "checked-out", label: "Checked out" },
         ]}
       />
 
       <SortBy
         options={[
-          { value: "startDate-desc", label: "Sort by date (recent first)" },
-          { value: "startDate-asc", label: "Sort by date (earlier first)" },
+          { value: "startDate-desc", label: "Booking date (newest)" },
+          { value: "startDate-asc", label: "Booking date (oldest)" },
           {
             value: "totalPrice-desc",
-            label: "Sort by amount (high first)",
+            label: "Sort by amount (highest)",
           },
-          { value: "totalPrice-asc", label: "Sort by amount (low first)" },
+          { value: "totalPrice-asc", label: "Sort by amount (lowest)" },
         ]}
       />
     </TableOperations>
