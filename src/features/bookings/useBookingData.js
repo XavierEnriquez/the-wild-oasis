900753433838e7dcd7f2b776b01f1ab24e52e597
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-12 23:41:27 -0600

    Create useBookingData.js

---
+++ src/features/bookings/useBookingData.js
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { getBookingFn } from "../../services/apiBookings";
+import { useParams } from "react-router-dom";
+
+export function useBookingData() {
+  const { bookingId } = useParams();
+
+  const {
+    data: booking,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["booking"],
+    queryFn: () => getBookingFn(bookingId),
+    retry: false,
+  });
+  return { booking, isLoading, error };
+}
