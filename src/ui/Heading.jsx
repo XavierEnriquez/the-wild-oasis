f53c774db3f521398d00c80edc038e8ac9447f5d
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-17 12:50:51 -0600

    Update Heading.jsx

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -1,8 +1,43 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Heading = styled.h1`
-  font-size: 4rem;
-  font-weight: 600;
+  ${(props) =>
+    props.as === "h1" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+    `}
+
+  ${(props) =>
+    props.as === "h2" &&
+    css`
+      font-size: 2.5rem;
+      font-weight: 600;
+    `}
+    
+    ${(props) =>
+    props.as === "h3" &&
+    css`
+      font-size: 2rem;
+      font-weight: 600;
+    `}
+    ${(props) =>
+    props.as === "h3_center" &&
+    css`
+      font-size: 2rem;
+      font-weight: 600;
+      text-align: center;
+    `}
+    
+    ${(props) =>
+    props.as === "h4" &&
+    css`
+      font-size: 1.5rem;
+      font-weight: 600;
+      text-align: center;
+    `}
+      
+  line-height: 1.4;
 `;
 
 export default Heading;
