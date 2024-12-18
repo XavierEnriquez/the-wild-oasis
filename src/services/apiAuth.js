51462947674f1726bc35281a65e3f7d861663d97
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-18 14:02:49 -0600

    Update apiAuth.js

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -8,6 +8,23 @@ export async function loginApi({ email, password }) {
 
   if (error) throw new Error(error.message);
 
-  console.log(data);
+  //   console.log(data);
   return data;
+}
+
+export async function getCurrentUser() {
+  const { data: session } = await supabase.auth.getSession();
+
+  if (!session.session) return null;
+
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error) throw new Error(error.message);
+
+  return data.user;
+}
+
+export async function logoutApi() {
+  const { error } = await supabase.auth.signOut();
+  if (error) throw new Error(error.message);
 }
