84b3003918b383b70e482e1b2806ef5a7efb562e
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-19 11:52:58 -0600

    Update UpdateUserDataForm.jsx

--- src/features/authentication/UpdateUserDataForm.jsx
+++ src/features/authentication/UpdateUserDataForm.jsx
@@ -7,7 +7,10 @@ import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 
 import { useUser } from "./useUser";
+import { useUpdateUser } from "./useUpdateUser";
 
+/*
+// Lecture code
 function UpdateUserDataForm() {
   // We don't need the loading state, and can immediately use the user data, because we know that it has already been loaded at this point
   const {
@@ -17,11 +20,28 @@ function UpdateUserDataForm() {
     },
   } = useUser();
 
+  const { updateUser, isUpdating } = useUpdateUser();
+
   const [fullName, setFullName] = useState(currentFullName);
   const [avatar, setAvatar] = useState(null);
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!fullName) return;
+    updateUser(
+      { fullName, avatar },
+      {
+        onSuccess: () => {
+          setAvatar(null);
+          e.target.reset();
+        },
+      }
+    );
+  }
+
+  function handleCancel() {
+    setFullName(currentFullName);
+    setAvatar(null);
   }
 
   return (
@@ -35,6 +55,7 @@ function UpdateUserDataForm() {
           value={fullName}
           onChange={(e) => setFullName(e.target.value)}
           id="fullName"
+          disabled={isUpdating}
         />
       </FormRow>
       <FormRow label="Avatar image">
@@ -42,13 +63,91 @@ function UpdateUserDataForm() {
           id="avatar"
           accept="image/*"
           onChange={(e) => setAvatar(e.target.files[0])}
+          disabled={isUpdating}
         />
       </FormRow>
       <FormRow>
-        <Button type="reset" variation="secondary">
+        <Button
+          type="reset"
+          onClick={handleCancel}
+          variation="secondary"
+          disabled={isUpdating}
+        >
           Cancel
         </Button>
-        <Button>Update account</Button>
+        <Button disabled={isUpdating}>Update account</Button>
+      </FormRow>
+    </Form>
+  );
+}
+*/
+
+// Code with added avatar functionality
+function UpdateUserDataForm() {
+  // We don't need the loading state, and can immediately use the user data, because we know that it has already been loaded at this point
+  const {
+    user: {
+      email,
+      user_metadata: { fullName: currentFullName, avatar: currentAvatar },
+    },
+  } = useUser();
+
+  const { updateUser, isUpdating } = useUpdateUser();
+
+  const [fullName, setFullName] = useState(currentFullName);
+  const [newAvatar, setNewAvatar] = useState(null);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!fullName) return;
+    updateUser(
+      { fullName, currentAvatar, newAvatar },
+      {
+        onSuccess: () => {
+          setNewAvatar(null);
+          e.target.reset();
+        },
+      }
+    );
+  }
+
+  function handleCancel() {
+    setFullName(currentFullName);
+    setNewAvatar(null);
+  }
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormRow label="Email address">
+        <Input value={email} disabled />
+      </FormRow>
+      <FormRow label="Full name">
+        <Input
+          type="text"
+          defaultValue={fullName}
+          onChange={(e) => setFullName(e.target.value)}
+          id="fullName"
+          disabled={isUpdating}
+        />
+      </FormRow>
+      <FormRow label="Avatar image">
+        <FileInput
+          id="avatar"
+          accept="image/*"
+          onChange={(e) => setNewAvatar(e.target.files[0])}
+          disabled={isUpdating}
+        />
+      </FormRow>
+      <FormRow>
+        <Button
+          type="reset"
+          onClick={handleCancel}
+          variation="secondary"
+          disabled={isUpdating}
+        >
+          Cancel
+        </Button>
+        <Button disabled={isUpdating}>Update account</Button>
       </FormRow>
     </Form>
   );
