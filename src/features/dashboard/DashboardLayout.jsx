9c70f2916c7a65c059d93544ee64416490ce46bc
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-19 16:45:40 -0600

    Update DashboardLayout.jsx

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,16 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today Activity</div>
+      <div>Stay Duration Chart</div>
+      <div>Sales Chart</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
