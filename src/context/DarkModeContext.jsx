97418eab6065460233b37d2751abd3d3f28bf95a
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-19 16:46:09 -0600

    Create DarkModeContext.jsx

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+import { useEffect } from "react";
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+import { DarkModeContext } from "./useDarkMode";
+
+export function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+
+  useEffect(
+    function () {
+      if (isDarkMode) {
+        document.documentElement.classList.add("dark-mode");
+        document.documentElement.classList.remove("light-mode");
+      } else {
+        document.documentElement.classList.remove("dark-mode");
+        document.documentElement.classList.add("light-mode");
+      }
+    },
+    [isDarkMode]
+  );
+
+  function toggleDarkMode() {
+    setIsDarkMode((isDark) => !isDark);
+  }
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
