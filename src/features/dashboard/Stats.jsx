70d7e604de80b7293f6779870a7c2d3ce86fb1d2
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-12-20 15:27:28 -0600

    Create Stats.jsx

---
+++ src/features/dashboard/Stats.jsx
@@ -0,0 +1,60 @@
+/* eslint-disable react/prop-types */
+
+import {
+  HiOutlineBanknotes,
+  HiOutlineBriefcase,
+  HiOutlineCalendarDays,
+  HiOutlineChartBar,
+} from "react-icons/hi2";
+import Stat from "./Stat";
+import { formatCurrency } from "../../utils/helpers";
+
+function Stats({ bookings, confirmedStays, numDays, cabinCount }) {
+  // Bookings
+  const numBookings = bookings?.length;
+
+  // Sales
+  const sales = bookings?.reduce((acc, cur) => acc + cur.totalPrice, 0);
+
+  // Check ins
+  const checkins = confirmedStays?.length;
+
+  // This formula is for this demo app and may NOT be accuarte for a true occupancy rate.
+  // Occupancy Rate = (Number of booked nights in a period of (7, 30, or 90 days)) / (Total Number of daily Available cabins * same period of (7, 30, or 90 days)) × 100
+  const ocupancyRate = Math.round(
+    (confirmedStays.reduce((acc, curr) => acc + curr.numNights, 0) /
+      (cabinCount * numDays)) *
+      100
+  );
+
+  return (
+    <>
+      <Stat
+        title="Bookings"
+        color="blue"
+        icon={<HiOutlineBriefcase />}
+        value={numBookings}
+      />
+      <Stat
+        title="Sales"
+        color="green"
+        icon={<HiOutlineBanknotes />}
+        value={formatCurrency(sales)}
+      />
+      <Stat
+        title="Check ins"
+        color="indigo"
+        icon={<HiOutlineCalendarDays />}
+        value={checkins}
+      />
+      <Stat
+        title="Occupancy Rate"
+        color="yellow"
+        icon={<HiOutlineChartBar />}
+        value={ocupancyRate + "%"}
+      />
+    </>
+  );
+}
+
+export default Stats;
